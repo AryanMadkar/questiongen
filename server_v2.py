2a2d319873529b79594a3e0562959c6c5daac65e
Aryan Madkar <164667750+AryanMadkar@users.noreply.github.com>
2025-06-03 19:45:49 +0530

    Update server_v2.py

--- server_v2.py
+++ server_v2.py
@@ -22,7 +22,7 @@ load_dotenv()  # Loads the .env file variables into environment variables
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-httpx_client = httpx.Client(proxies=None)
+httpx_client = httpx.Client()
 
 app = Flask(__name__)
 api_key = os.getenv('API_KEY')
